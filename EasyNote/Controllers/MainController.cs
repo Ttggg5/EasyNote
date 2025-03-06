4e041030c3353f47b2e904e662760826ba8d0c1c
Ttggg5 <s95510691@gmail.com>
2025-03-06 17:07:55 +0800

    start page complete

--- EasyNote/Controllers/MainController.cs
+++ EasyNote/Controllers/MainController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
+using Google.Apis.Auth;
 
 namespace EasyNote.Controllers
 {
@@ -48,6 +49,104 @@ namespace EasyNote.Controllers
             var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
             HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
             return Redirect("/Workspace");
+        }
+
+        /// <summary>
+        /// 驗證 Google 登入授權
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GoogleLogin()
+        {
+            try
+            {
+                string? formCredential = Request.Form["credential"]; //回傳憑證
+                string? formToken = Request.Form["g_csrf_token"]; //回傳令牌
+                string? cookiesToken = Request.Cookies["g_csrf_token"]; //Cookie 令牌
+
+                // 驗證 Google Token
+                GoogleJsonWebSignature.Payload? payload = VerifyGoogleToken(formCredential, formToken, cookiesToken).Result;
+                if (payload == null)
+                {
+                    // 驗證失敗
+                    TempData["error"] = "unexpected error!";
+                    return Redirect("/Index");
+                }
+                else
+                {
+                    //驗證成功，取使用者資訊內容
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Email, payload.Email),
+                        new Claim(ClaimTypes.Name, payload.Name),
+                        new Claim(ClaimTypes.Role, "User")
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                    return Redirect("/Workspace");
+                }
+            }
+            catch {
+                TempData["error"] = "unexpected error!";
+                return Redirect("/Index");
+            }
+        }
+
+        /// <summary>
+        /// 驗證 Google Token
+        /// </summary>
+        /// <param name="formCredential"></param>
+        /// <param name="formToken"></param>
+        /// <param name="cookiesToken"></param>
+        /// <returns></returns>
+        public async Task<GoogleJsonWebSignature.Payload?> VerifyGoogleToken(string? formCredential, string? formToken, string? cookiesToken)
+        {
+            // 檢查空值
+            if (formCredential == null || formToken == null && cookiesToken == null)
+            {
+                return null;
+            }
+
+            GoogleJsonWebSignature.Payload? payload;
+            try
+            {
+                // 驗證 token
+                if (formToken != cookiesToken)
+                {
+                    return null;
+                }
+
+                // 驗證憑證
+                IConfiguration Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+                string GoogleApiClientId = Config.GetSection("GoogleApiClientId").Value;
+                var settings = new GoogleJsonWebSignature.ValidationSettings()
+                {
+                    Audience = new List<string>() { GoogleApiClientId }
+                };
+                payload = await GoogleJsonWebSignature.ValidateAsync(formCredential, settings);
+                if (!payload.Issuer.Equals("accounts.google.com") && !payload.Issuer.Equals("https://accounts.google.com"))
+                {
+                    return null;
+                }
+                if (payload.ExpirationTimeSeconds == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    DateTime now = DateTime.Now.ToUniversalTime();
+                    DateTime expiration = DateTimeOffset.FromUnixTimeSeconds((long)payload.ExpirationTimeSeconds).DateTime;
+                    if (now > expiration)
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            return payload;
         }
 
         public IActionResult Workspace()
